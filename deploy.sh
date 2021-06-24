f1ca73ba74077a2dc5ca8c2fd312a5c6ad6f6269
mike-dt <kimyoungmichael2@gmail.com>
2021-06-24 16:38:05 -0400

    test data added. deploy.sh file created

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#To make a FRESH deployment, do the following: 
+#Git (instructions below from https://gist.github.com/CristinaSolana/1885435)
+#git clone git@github.com/kimyoungmichael2/lpdb.git
+#git remote add upstream git@github.com/hanbeul/lpdb.git
+#git fetch upstream
+#git pull upstream develop (or whatever branch you want to pull)
+#merge changes as needed. 
+
+#To apply changes to pre-existing deployment and redeploy, do the following:
+#git fetch upstream
+#git pull upstream develop (or whatever branch you want to pull)
+#merge changes as needed. 
