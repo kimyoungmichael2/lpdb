a875a573403d6c0f242f1136e73e68da26c38f02
Michael Kim <kimyoungmichael2@gmail.com>
2021-06-29 09:13:29 -0400

    updated deploy notes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #To make a FRESH deployment, do the following: 
-#Git (instructions below from https://gist.github.com/CristinaSolana/1885435)
+#Instructions below from https://gist.github.com/CristinaSolana/1885435)
 #git clone git@github.com/kimyoungmichael2/lpdb.git
 #git remote add upstream git@github.com/hanbeul/lpdb.git
 #git fetch upstream
@@ -12,6 +12,9 @@
 #git fetch upstream
 #git pull upstream develop (or whatever branch you want to pull)
 #merge changes as needed. 
+
+#Once merging is done, run "npm run build" in the frontend folder.
+#Once building is complete, run "sudo bash deploy.sh" in the project root directory.
 
 cp -r ./frontend/build/. /var/www/michaelyoung.kim/lpdb/
 echo files copied!
